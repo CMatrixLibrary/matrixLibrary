f347bf2e529ffe107a5a878e80c1a38a07a9d44a
PiotrTrawinski <38530033+PiotrTrawinski@users.noreply.github.com>
2019-02-20 21:58:19 +0100

    Implemented Range class (for Python-like ranges)

---
+++ Range.h
@@ -0,0 +1,50 @@
+#pragma once
+
+template<typename T> class Range {
+    class Iterator {
+    public:
+        Iterator(T value) : value(value) {}
+
+        Iterator& operator++() {
+            ++value;
+            return *this;
+        }
+        bool operator==(const Iterator& other) {
+            return value == other.value;
+        }
+        bool operator!=(const Iterator& other) {
+            return value != other.value;
+        }
+        int operator*() {
+            return value;
+        }
+
+    private:
+        T value;
+    };
+
+public:
+    Range(T start, T end) :
+        start_(start),
+        end_(end)
+    {}
+    Range(T end) :
+        start_(0),
+        end_(end)
+    {}
+
+    Iterator begin() {
+        return Iterator(start_);
+    }
+    Iterator end() {
+        return Iterator(end_);
+    }
+
+private:
+    T start_;
+    T end_;
+};
+
+template<typename Container> auto indicies(const Container& container) -> Range<decltype(container.size())> {
+    return Range<decltype(container.size())>(0, container.size());
+}
